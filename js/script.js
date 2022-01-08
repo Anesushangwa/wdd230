acb7830229b613b26a7a158566b625607e0f0abe
Anesushangwa <sha21032@byui.edu>
2022-01-08 03:41:57 -0800

    add file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// const options = {weekday:'long' , day: 'numeric', month: 'long', year: 'numeric' };
+
+const lastmodify = "Last modify:";
+
+let oLastModif = new Date(document.lastModified);
+
+const text = `${ lastmodify}, ${oLastModif }`;
+
+document.getElementById("updatetime").innerHTML = text;
