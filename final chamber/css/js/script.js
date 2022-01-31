0281f76dee76ca0ae2debd8166662cb25972c170
Anesushangwa <sha21032@byui.edu>
2022-01-31 12:47:24 +0200

    new

--- final chamber/css/js/script.js
+++ final chamber/css/js/script.js
@@ -8,3 +8,30 @@ menubutton.addEventListener('click', () => {
     exitbutton.classList.toggle('hidden');
     ham.classList.toggle('hidden');
 }, );
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector("date");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+
+
+const lastmodify = "Last modified:";
+
+let oLastModif = new Date(document.lastModified);
+
+const text = `${ lastmodify} ${oLastModif }`;
+
+document.getElementById("updatetime").innerHTML = text;
