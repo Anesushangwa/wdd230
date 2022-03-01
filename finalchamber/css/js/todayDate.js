aa7ce81958d340c135c669a3bde8c6126deb8409
Anesushangwa <sha21032@byui.edu>
2022-03-01 16:29:51 +0200

    new

--- finalchamber/css/js/todayDate.js
+++ finalchamber/css/js/todayDate.js
@@ -12,3 +12,18 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
 
+// greetings
+const time = new Date().getHours();
+let greeting;
+if (time < 10) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+document.getElementById("greeting").innerHTML = greeting;
+
+
+// var x = document.getElementById("myImg").src;
+// document.getElementById("demo").innerHTML = x;
